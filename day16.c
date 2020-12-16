aa57f3f81ec5d296855948ac3846e74d2843bc2d
Wolfgang Steiner <wolfgang.steiner@gmail.com>
2020-12-16 07:15:45 +0100

    Add day16.c

---
+++ day16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "unity.h"
+#include <string.h>
+#include <stdbool.h>
+#include <assert.h>
+#include "util.h"
+
+
+void setUp()
+{
+}
+
+
+void tearDown()
+{
+}
+
+
+int main(int argc, char** argv)
+{
+    UNITY_BEGIN();
+    UNITY_END();
+
+    return 0;
+}
+
