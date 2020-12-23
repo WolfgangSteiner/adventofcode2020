23f8bbee3556d7862cfed553eb4223bc896e62b3
Wolfgang Steiner <wolfgang.steiner@gmail.com>
2020-12-23 17:57:54 +0100

    Add day23.c

---
+++ day23.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "unity.h"
+#include <string.h>
+#include <stdbool.h>
+#include <assert.h>
+#include "util.h"
+
+void setUp()
+{
+}
+
+
+void tearDown()
+{
+}
+
+
+int main()
+{
+    UNITY_BEGIN();
+    UNITY_END();
+
+    return 0;
+}
+
